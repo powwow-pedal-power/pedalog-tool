3761f08260d617a5231d3f9910d8cc3cf3a16e5f
Dan Haughey <dan@greenlynx.org.uk>
2011-07-14 11:59:45 +0100

    Added command-line arguments for comma-separated display and serial number filtering

--- src/pedalog-tool.c
+++ src/pedalog-tool.c
@@ -24,35 +24,108 @@
 #include <string.h>
 #include <unistd.h>
 #include <pedalog.h>
+#include <argp.h>
+
+const char *argp_program_version = "pedalog-tool 0.1";
+const char *argp_program_bug_address = "<dan@powwow-pedal-power.org.uk>";
+
+static char doc[] = "Pedalog Tool - a simple program for reading data from Pedalog devices";
+
+/* The options we understand. */
+static struct argp_option options[] = {
+    {"comma",  'c', 0,      0,  "Produce comma-separated output" },
+    {"serial",   's', "SERIAL", 0,
+        "Read data from a Pedalog device with a specific serial number" },
+    { 0 }
+};
+
+/* A description of the arguments we accept. */
+static char args_doc[] = "";
+
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+    int comma;
+    int serial;
+};
+
+/* Parse a single option. */
+static error_t parse_opt (int key, char *arg, struct argp_state *state)
+{
+    /* Get the input argument from argp_parse, which we
+       know is a pointer to our arguments structure. */
+    struct arguments *arguments = state->input;
+
+    switch (key)
+    {
+        case 'c':
+            arguments->comma = 1;
+            break;
+        case 's':
+            arguments->serial = atoi(arg);
+            if (arguments->serial < 1) {
+                argp_error(state, "option 'serial', if specified, must be a positive integer");
+                return EINVAL;
+            }
+
+            break;
+
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+/* Our argp parser. */
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int display_error(int error)
 {
     char message[PEDALOG_MAX_ERROR_MESSAGE];
     pedalog_get_error_message(error, message);
 
-    printf("%s\n", message);
+    fprintf(stderr, "%s\n", message);
     return error;
 }
 
-int main()
+int main(int argc, char **argv)
 {
+    struct arguments arguments;
+
+    /* Default values. */
+    arguments.comma = 0;
+    arguments.serial = 0;
+
+    /* Parse our arguments; every option seen by parse_opt will
+       be reflected in arguments. */
+    argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
     pedalog_init();
 
     pedalog_data data;
     pedalog_device devices[PEDALOG_MAX_DEVICES];
 
     int device_count = pedalog_find_devices(devices);
-    if (device_count < 1) {
-        return display_error(PEDALOG_ERROR_NO_DEVICE_FOUND);
+
+    int i;
+    for (i = 0; i < device_count; ++i) {
+        if (arguments.serial == 0 || arguments.serial == devices[i].serial) {
+            int result = pedalog_read_data(&devices[i], &data);
+
+            if (result == PEDALOG_OK) {
+                if (arguments.comma) {
+                    printf("%d,%f,%f,%f,%f,%f,%f,%d\n",
+                        devices[i].serial, data.voltage, data.current, data.power, data.energy, data.max_power, data.avg_power, data.time);
+                } else {
+                    printf("Serial: %d, voltage: %f, current: %f, power: %f, energy: %f, max_power: %f, avg_power: %f, time: %d\n",
+                        devices[i].serial, data.voltage, data.current, data.power, data.energy, data.max_power, data.avg_power, data.time);
+                }
+                return 0;	
+            } else {
+                return display_error(result);
+            }
+        }
     }
 
-    int result = pedalog_read_data(&devices[0], &data);
-
-    if (result == PEDALOG_OK) {
-        printf("voltage: %f, current: %f, power: %f, energy: %f, max_power: %f, avg_power: %f, time: %ld\n",
-            data.voltage, data.current, data.power, data.energy, data.max_power, data.avg_power, data.time);
-        return 0;	
-    } else {
-        return display_error(result);
-    }
+    return display_error(PEDALOG_ERROR_NO_DEVICE_FOUND);
 }
